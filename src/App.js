62cbc4fd686e9256476ae1201c2d512f27b113fe
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-07-12 12:48:25 +0100

    trailling comma deleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 function App() {
   const [projects, setProjects] = useState({});
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/bearkillerPT/repos-website/main/public/projects.json').then( (res)=>
+      console.log(res.json())
+    )
+    return () => {
+    }
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
